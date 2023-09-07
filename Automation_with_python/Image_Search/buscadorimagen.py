7e34f869e8313f769243f19b0986a6ba34e55401
DevOkana <figueredojesus@outlook.es>
2023-09-07 17:10:45 +0200

    Updating comments in the code

--- Automation_with_python/Image_Search/buscadorimagen.py
+++ Automation_with_python/Image_Search/buscadorimagen.py
@@ -1,33 +1,37 @@
 import os
 import shutil
 
-def procesar_carpeta(carpeta_actual, carpeta_destino):
-    # Busca recursivamente en todas las subcarpetas de la carpeta actual
-    for dirpath, dirnames, filenames in os.walk(carpeta_actual):
+
+def process_folder(current_folder, destination_folder):
+    # Recursively searches all subfolders of the current folder
+    for dirpath, dirnames, filenames in os.walk(current_folder):
         for filename in filenames:
-            if filename.endswith('.jpg', '.jpeg', '.png', '.gif', '.bmp'):
-                # Copia el archivo a la carpeta de destino
-                ruta_original = os.path.join(dirpath, filename)
-                ruta_nueva = os.path.join(carpeta_destino, filename)
-                
+            if filename.endswith(".jpg", ".jpeg", ".png", ".gif", ".bmp"):
+                # Copy the file to the destination folder
+                path_origin = os.path.join(dirpath, filename)
+                path_new = os.path.join(destination_folder, filename)
+
                 try:
-                    shutil.copy2(ruta_original, ruta_nueva)
+                    shutil.copy2(path_origin, path_new)
                 except Exception as e:
-                    print(f"No se pudo copiar el archivo {ruta_original}: {str(e)}")
+                    print(f"Unable to copy file  {path_origin}: {str(e)}")
 
-    # Procesa las subcarpetas de manera recursiva
+    # Processes the subfolders recursively
     for subdir in dirnames:
-        nueva_carpeta_actual = os.path.join(carpeta_actual, subdir)
-        nueva_carpeta_destino = os.path.join(carpeta_destino, subdir)
-        procesar_carpeta(nueva_carpeta_actual, nueva_carpeta_destino)
+        new_current_folder = os.path.join(current_folder, subdir)
+        new_destination_folder = os.path.join(destination_folder, subdir)
+        process_folder(new_current_folder, new_destination_folder)
 
-# Carpeta inicial a procesar
-carpeta_inicial = '.'  # Puedes reemplazar '.' con la ruta absoluta de la carpeta inicial
 
-# Carpeta de destino para almacenar las imágenes
-carpeta_destino = 'imagenes_organizadas'
-if not os.path.exists(carpeta_destino):
-    os.makedirs(carpeta_destino)
+# Start folder to process
+folder_initial = (
+    "."  # You can replace '.' with the absolute path of the initial folder.
+)
 
-# Procesa la carpeta inicial
-procesar_carpeta(carpeta_inicial, carpeta_destino)
+# Destination folder to store images
+destination_folder = "images_organized"
+if not os.path.exists(destination_folder):
+    os.makedirs(destination_folder)
+
+#  Process the initial folder
+process_folder(folder_initial, destination_folder)
